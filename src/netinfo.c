30c978c58dd8e3f11aa6112f875e52a1759ad815
Ravenmorph <spinalcordsword@gmail.com>
2025-08-18 09:05:52 -0400

    Read MAC

--- src/netinfo.c
+++ src/netinfo.c
@@ -13,3 +13,22 @@
 #include <errno.h>
 
 #include "netinfo.h"
+
+
+
+/* Read MAC for an interface name using ioctl SIOCGIFHWADDR (Linux) */
+static int get_mac_ioctl(const char *ifname, char *mac_str, size_t mac_str_len) {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) return -1;
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    strncpy(ifr.ifr_name, ifname, IFNAMSIZ-1);
+    if (ioctl(fd, SIOCGIFHWADDR, &ifr) == -1) {
+        close(fd);
+        return -1;
+    }
+    unsigned char *mac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
+    mac_to_str(mac, mac_str, mac_str_len);
+    close(fd);
+    return 0;
+}
