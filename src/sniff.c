25cd3c51f65271047c44b99ab04fa9d07af56ac8
Ravenmorph <spinalcordsword@gmail.com>
2025-08-19 14:23:47 -0400

    Simple Hexdump

--- src/sniff.c
+++ src/sniff.c
@@ -18,3 +18,23 @@ static void handle_sigint(int sig) {
         pcap_breakloop(pcap_handle);
     }
 }
+
+
+/* simple hexdump for first n bytes */
+static void small_hexdump(const u_char *data, int len, int max_bytes) {
+    int to = len < max_bytes ? len : max_bytes;
+    for (int i = 0; i < to; i += 16) {
+        printf("%04x  ", i);
+        for (int j = 0; j < 16; ++j) {
+            if (i + j < to) printf("%02x ", data[i + j]);
+            else printf("   ");
+            if (j == 7) printf(" ");
+        }
+        printf(" |");
+        for (int j = 0; j < 16 && i + j < to; ++j) {
+            unsigned char c = data[i + j];
+            printf("%c", (c >= 32 && c < 127) ? c : '.');
+        }
+        printf("|\n");
+    }
+}
