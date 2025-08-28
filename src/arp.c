bcff89870fb2bd43882fb4e34e09d2845b2f1387
Ravenmorph <spinalcordsword@gmail.com>
2025-08-28 14:00:55 -0400

    Simple struct to store discovered hosts

--- src/arp.c
+++ src/arp.c
@@ -121,3 +121,15 @@ static int parse_cidr(const char *cidr, uint32_t *base_net, int *prefix_len) {
     *prefix_len = plen;
     return 0;
 }
+
+
+/* Simple struct to store discovered hosts */
+struct arp_entry {
+    uint32_t ip; // network order
+    unsigned char mac[6];
+};
+
+static void mac_to_str(const unsigned char mac[6], char *out, size_t outlen) {
+    snprintf(out, outlen, "%02x:%02x:%02x:%02x:%02x:%02x",
+             mac[0],mac[1],mac[2],mac[3],mac[4],mac[5]);
+}
